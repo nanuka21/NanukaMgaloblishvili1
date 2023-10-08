df16c22f24cc28a56dcbc6f3e670c7412acc5df1
nanuka <nanuka@Nanukas-MacBook-Air.local>
2023-10-08 22:23:02 +0400

    add homework to Git hub

---
+++ სავარჯიშო/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+Console.WriteLine("Welcome to our game,which is about guessing numbers");
+Console.WriteLine("You have 3 tries, please enter the number if it matches the number we guessed you win otherwise unfortunately you lose");
+Console.WriteLine("Please enter the number > ");
+
+Random random = new Random();
+int randomNumber = random.Next (1, 11);
+int Numberoftries = 3;
+
+for (int i = 0; i < Numberoftries; i++)
+{
+    Console.Write($"Attemp {i + 1}:");
+    int userGuess = int.Parse(Console.ReadLine());
+
+    if (userGuess == randomNumber)
+    {
+        Console.WriteLine("Congretulation ,you won!");
+        break;
+    }
+    else
+
+        Console.WriteLine("Sorry , you lost Try again:(");
+
+    {
+        if (userGuess != randomNumber)
+            Console.WriteLine("you lose Finally bye bye the correct number was{randomNumber}.")
+;
+    }
+}
+    
