4acfdca3f9203c4ab28b8fbc66900bb9dd64d250
nanuka21 <71572530+nanuka21@users.noreply.github.com>
2023-10-16 22:36:05 +0400

    davaleba 4

---
+++ დავალება 4/დავალება N4/დავალება N4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 10, 12, 14, 17, 20 };
+        int sum = SumArrayValue(numbers, 2);
+        Console.WriteLine(sum);
+    }
+
+    static int SumArrayValue(int[] inArray, int index)
+    {
+        int sum = 0;
+        int ourNumber = inArray[index];
+        string numberAsString = ourNumber.ToString();
+        
+        for (int i = 0; i < numberAsString.Length; i++)
+        {
+            char digitChar = numberAsString[i];
+
+            if (char.IsDigit(digitChar))
+            {
+                int digit = int.Parse(digitChar.ToString());
+                sum += digit;
+            }
+        }
+
+        return sum;
+        //int sum = 0;
+        //if (index >= 0 && index < inArray.Length)
+        //{
+        //    int ourNumber = inArray[index];
+        //    return inArray[index];
+        //}
+        //else
+        //{
+        //    // You might want to handle the case when the index is out of bounds.
+        //    Console.WriteLine("Index is out of bounds.");
+        //    return 0;
+        //}
+    }
+}
+
+
